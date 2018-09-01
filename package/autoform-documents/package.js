c1b655a7bc59935565e08cbc42ac4a11f07ec25b
Jan Küster <jkuester@uni-bremen.de>
2018-09-01 21:04:32 +0200

    1.0.0

--- package/autoform-documents/package.js
+++ package/autoform-documents/package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'jkuester:autoform-documents',
-  version: '0.1.0',
+  version: '1.0.0',
   // Brief, one-line summary of the package.
   summary: 'Select or create documents and link them in your current form.',
   // URL to the Git repository containing the source code for this package.
@@ -11,15 +11,15 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.6.1.1');
+  api.versionsFrom('1.6');
   api.use('ecmascript');
-  api.use('templating');
+  api.use('templating@1.3.2');
   api.use('underscore');
   api.use('random');
   api.use('jquery');
   api.use('tracker');
   api.use('reactive-dict');
-  api.use('dburles:mongo-collection-instances');
+  api.use('dburles:mongo-collection-instances@0.3.5');
   api.use('aldeed:autoform@6.3.0');
   api.addFiles([
     'autoform-documents.html',
