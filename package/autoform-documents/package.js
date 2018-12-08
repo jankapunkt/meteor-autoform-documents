f16a971027d9da6566fd8bd3696f5e1d746b9471
Jan Küster <jkuester@uni-bremen.de>
2018-12-08 21:12:21 +0100

    1.4.0

--- package/autoform-documents/package.js
+++ package/autoform-documents/package.js
@@ -1,7 +1,7 @@
 /* eslint-env meteor */
 Package.describe({
   name: 'jkuester:autoform-documents',
-  version: '1.3.1',
+  version: '1.4.0',
   // Brief, one-line summary of the package.
   summary: 'Select or create documents and link them in your current form.',
   // URL to the Git repository containing the source code for this package.
@@ -15,13 +15,13 @@ Package.onUse(function (api) {
   api.versionsFrom('1.7')
   api.use('ecmascript')
   api.use('templating@1.3.2')
-  api.use('underscore')
   api.use('random')
   api.use('tracker')
   api.use('reactive-dict')
   api.use('dburles:mongo-collection-instances@0.3.5')
-  api.use('aldeed:autoform@6.3.0')
+  api.use('aldeed:autoform@6.0.0')
   api.addFiles([
+    'autoform.js',
     'autoform-documents.html',
     'autoform-documents.css',
     'autoform-documents.js'
