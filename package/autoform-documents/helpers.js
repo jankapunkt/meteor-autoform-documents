f369238a811c88c649d508b712307047792a6863
Jan Küster <jkuester@uni-bremen.de>
2018-12-08 14:55:05 +0100

    added formIsValid helper to check form and add sticky validation errors

---
+++ package/autoform-documents/helpers.js
@@ -0,0 +1,21 @@
+/* global AutoForm */
+
+export const formIsValid = function formIsValid (schema, formId, isUpdate) {
+  const formDoc = isUpdate
+    ? AutoForm.getFormValues(formId).updateDoc
+    : AutoForm.getFormValues(formId).insertDoc
+  let options
+  if (isUpdate) {
+    options = {}
+    options.modifier = true
+  }
+  const context = schema.newContext()
+  context.validate(formDoc, options)
+  const errors = context.validationErrors()
+  if (errors && errors.length > 0) {
+    errors.forEach(err => AutoForm.addStickyValidationError(formId, err.key, err.type, err.value))
+    return null
+  } else {
+    return formDoc
+  }
+}
